68708444058b7b4888e6737b51def3f8fbc6e690
Ricardo Espindola <86207903+RicardoEspindol@users.noreply.github.com>
2023-05-11 20:11:15 -0300

    init actions

---
+++ ex01/App.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import { StyleSheet, TouchableOpacity, Text, View, TextInput, ScrollView} from 'react-native';
+
+export default function App() {
+
+  const [listaDeTextos, setListaDeTextos] = useState([]);
+  const [novoTexto, setNovoTexto] = useState('');
+
+  return (
+    <View style={styles.container}>
+    <View style={styles.boxTitle}>
+            <Text style={styles.textTitle}>LISTA</Text>
+        </View>
+      <View style={styles.gridv}>
+        <TextInput
+          style={styles.input}
+          value={novoTexto}
+          onChangeText={setNovoTexto}
+        />
+        <TouchableOpacity style={styles.button}
+           onPress={() => {
+            if(novoTexto != '' && !listaDeTextos.includes(novoTexto)){
+              setListaDeTextos([...listaDeTextos, novoTexto]);
+            setNovoTexto('');
+            }else{
+              setListaDeTextos([...listaDeTextos])
+            }
+           
+          }}>
+            <Text style={styles.buttonText}>Adicionar</Text>
+          </TouchableOpacity>
+      </View>
+
+      <ScrollView style={styles.pai}>
+        {listaDeTextos.length === 0 ? (<Text style={styles.textTitle2}>A lista está vazia!</Text>) :
+
+          (listaDeTextos.map((texto, index) => (
+          <View key={index} style={styles.gridv}>
+            <Text style={styles.texto}>{texto}</Text>
+            <View>
+              <TouchableOpacity style={styles.button2}
+                onPress={() => {
+                  setListaDeTextos(listaDeTextos.filter((texto, indexAtual) => indexAtual !== index));
+                }}>
+                <Text style={styles.buttonText}>Excluir</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 190,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginBottom: 50,
+  },
+  texto: {
+    fontSize: 18,
+    marginBottom: 10,
+    marginTop: 10,
+    borderRadius: 5,
+    width: '60%',
+  },
+  gridv: {
+      flexDirection: 'row',
+      justifyContent:'space-around',
+      alignItems: 'center',
+    },
+    button: {
+      backgroundColor: '#248a05',
+      padding: 10,
+      borderRadius: 5,
+      marginLeft: 25,
+      marginBottom: 50
+    },
+    button2: {
+      backgroundColor: '#dd1616',
+      padding: 10,
+      borderRadius: 5,
+      marginLeft: 25,
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    boxTitle: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: 50,
+    },
+    textTitle: {
+        color: '#FF0043',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    textTitle2:{
+      color: '#FF0043',
+      fontSize: 20,
+      fontWeight: '400'
+    }
+});
